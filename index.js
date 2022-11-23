9c271e60eaca4a4029c27ebfe9767439c967c995
LAPTOP-Q7NE4MO3\kepco <mas_jaypark@naver.com>
2022-11-23 14:00:35 +0900

    Node js 와 Express js 다운로드. index.js 생성 및 package.json 생성, 해당 내용 작성

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js 는 백엔드 시작점이라고 생각
+// package.json 에서, script 부분 , start 하면 node 앱 시작. 시작점은 index .js
+
+
+const express = require('express') // express module 가져옴
+const app = express() // express function 을 이용해 새로운 app 을 만듬
+const port = 5000 // port 번호는 딱히 상관 없음
+
+app.get('/', (req,res) => res.send('Hello World!')) // root 디렉토리에오면 문자열 출력
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`)) 
+// port 5000 에서 해당 앱 실행
+// 해당 앱이 실행되면, 문자열 출력
